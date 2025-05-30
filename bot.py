26f53fe9a88156e25f7a21ed832c79a982fc6954
Marvin Tetteh <131989527+ScorpionLordbot@users.noreply.github.com>
2025-05-30 02:20:49 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,17 @@
+import os
 import asyncio
 from telegram import Bot
 from datetime import datetime
 
-BOT_TOKEN = '8018996550:AAECewNhwNkW0ue2sx0CKeTUc4QN3nYq7cg'
-CHANNEL_USERNAME = '@deadlysync100'
+BOT_TOKEN = os.environ['BOT_TOKEN']
+CHANNEL_ID = os.environ['CHANNEL_ID']
 
 bot = Bot(token=BOT_TOKEN)
 
-async def post_regularly():
+async def post():
     while True:
-        message = f"📢 Hello from PythonAnywhere!\n🕒 {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-        await bot.send_message(chat_id=CHANNEL_USERNAME, text=message)
-        await asyncio.sleep(3600)  # Every hour
+        message = f"🚀 Hello from Railway!\n🕒 {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        await bot.send_message(chat_id=CHANNEL_ID, text=message)
+        await asyncio.sleep(3600)  # every hour
 
-async def main():
-    print("Bot started...")
-    await post_regularly()
-
-asyncio.run(main())
+asyncio.run(post())
